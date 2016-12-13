09674595bdba2ef1c685615dcaa40753b815a669
timbo05sec <timbo05sec@gmail.com>
2016-12-13 14:28:02 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,7 +30,7 @@ if [ "$color_prompt" = yes ]; then
     PS1='${debian_chroot:+($debian_chroot)}\[\033[01;31m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]:\D{%F-%T}~\$'
 else
     PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w:\D{%F-%T}~\$'
-fi' >> ~/.basbrc
+fi' >> ~/.bashrc
 
 echo '# Logs all terminal output
 # Eg. ~/.terminal-logs/2015-06-02_08:15:25.14622.log
