92a55f6d3ead0c357075f3b7550ea89e80622e54
timbo05sec <timbo05sec@gmail.com>
2017-08-14 13:33:21 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ apt-get install telnet
 apt-get install dnsrecon
 apt-get install masscan
 apt-get install net-tools
+apt-get install theharvester
 pip install pyip
 git clone https://github.com/SpiderLabs/ikeforce.git /home/admin/downloads/ikeforce
 git clone https://bitbucket.org/LaNMaSteR53/recon-ng.git /home/admin/downloads/recon-ng
