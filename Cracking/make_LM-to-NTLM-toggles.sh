7bb73d71215e0ed5f330197659ad8d5d605af0b6
Timothy McKenzie <timbo05sec@gmail.com>
2022-02-23 12:50:45 +0700

    Create make_LM-to-NTLM-toggles.sh

---
+++ Cracking/make_LM-to-NTLM-toggles.sh
@@ -0,0 +1,7 @@
+for teeone in {0..6}; do echo T$teeone; done; 
+for teeone in {0..6}; do for teetwo in {0..6}; do echo T$teeone\T$teetwo; done; done;
+for teeone in {0..6}; do for teetwo in {0..6}; do for teethree in {0..6}; do echo T$teeone\T$teetwo\T$teethree; done; done; done;
+for teeone in {0..6}; do for teetwo in {0..6}; do for teethree in {0..6}; do for teefour in {0..6}; do echo T$teeone\T$teetwo\T$teethree\T$teefour; done; done; done; done;
+for teeone in {0..6}; do for teetwo in {0..6}; do for teethree in {0..6}; do for teefour in {0..6}; do for teefive in {0..6}; do echo T$teeone\T$teetwo\T$teethree\T$teefour\T$teefive; done; done; done; done; done;
+for teeone in {0..6}; do for teetwo in {0..6}; do for teethree in {0..6}; do for teefour in {0..6}; do for teefive in {0..6}; do for teesix in {0..6}; do echo T$teeone\T$teetwo\T$teethree\T$teefour\T$teefive\T$teesix; done; done; done; done; done; done;
+for teeone in {0..6}; do for teetwo in {0..6}; do for teethree in {0..6}; do for teefour in {0..6}; do for teefive in {0..6}; do for teesix in {0..6}; do for teeseven in {0..6}; do echo T$teeone\T$teetwo\T$teethree\T$teefour\T$teefive\T$teesix\T$teeseven; done; done; done; done; done; done; done;
